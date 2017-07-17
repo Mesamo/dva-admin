63b1901f193c02c54af8822f1f1c098e9f53bc8f
mesamo <mesamo@foxmail.com>
2017-07-17 16:48:43 -0500

    feat: antd使用本地字体文件

---
+++ themes.js
@@ -0,0 +1,23 @@
+module.exports = () => {
+    return {
+        '@font-family': '"Encode Sans Narrow"',
+        '@font-size-base': '14px',
+        '@font-size-lg': '14px',
+        '@text-color': '#333',
+
+        '@line-height-base': '1.5',
+
+        '@border-radius-base': '4px',
+        '@border-radius-sm': '4px',
+
+        '@btn-padding-lg': '4px 15px 4px 15px',
+
+        '@primary-color': '#069165',
+        '@link-color': '#069165',
+
+        '@icon-url': '"/assets/iconfont/iconfont"',
+
+        '@input-height-lg': '32px',
+
+    };
+};
