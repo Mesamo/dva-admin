0880665a40c907e1d4d1d39dd2eabf9527df0b10
mesamo <mesamo@foxmail.com>
2017-08-15 18:43:23 -0500

    build: 更新打包方式

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,6 @@
-import firebase from 'firebase';
-// import firebase from 'firebase/app';
-// import 'firebase/auth';
-// import 'firebase/database';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
 
 const config = {
     projectId: 'dva-admin',
