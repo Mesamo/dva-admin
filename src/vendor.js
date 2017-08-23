980191e7b5dc273794651e913e89e9754c6c1982
mesamo <mesamo@foxmail.com>
2017-08-23 11:19:11 -0500

    build: 公共模块单独打包

---
+++ src/vendor.js
@@ -0,0 +1,20 @@
+import 'react'
+import 'react-dom'
+import 'react-router'
+import 'core-js'
+import 'rc-queue-anim';
+import 'prop-types'
+import 'path-to-regexp'
+import 'dva'
+import 'dva-loading'
+import 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
+
+import antd from 'antd'
+
+/* eslint-disable */
+const { Table, Form, Layout, Row, Col, Menu, Dropdown, Popover, Input, Button, Checkbox,
+  Card, Icon, Progress, Alert, Switch, Radio, Modal, message, notification
+} = antd
+/* eslint-disable */
